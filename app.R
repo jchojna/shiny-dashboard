aa24e46d03bf0ff038bf7262ef7b85f5ea9ac632
jchojna <jakub.chojna@gmail.com>
2020-05-08 14:14:34 +0200

    feat: add head tag and icons

--- app.R
+++ app.R
@@ -4,7 +4,41 @@ ui <- fluidPage(
   theme = "styles/main.css",
   class = "app",
   
-  # PAGE HEADER
+  # HEAD TAG ----
+  tags$head(
+    tags$meta(charset = "utf-8"),
+    tags$meta(name = "viewport", content = "width=device-width, initial-scale=1"),
+    tags$meta(
+      name = "shinyDashboard",
+      content = "Dashboard app created in R Shiny"
+    ),
+    tags$meta(property="og:title", content="shinyDashboard"),
+    tags$meta(
+      property="og:description",
+      content="Dashboard app created in R Shiny"
+    ),
+    tags$meta(property="og:type", content="website"),
+    tags$meta(property="og:url", content="https://jchojna.github.io/shiny-dashboard/"),
+    tags$meta(
+      property="og:image",
+      content="https://jchojna.github.io/shiny-dashboard/icons/icon-512x512.png"
+    ),
+    tags$meta(property="og:image:width", content="512"),
+    tags$meta(property="og:image:height", content="512"),
+    tags$meta(name="apple-mobile-web-app-capable", content="yes"),
+    
+    tags$link(
+      rel = "apple-touch-icon", sizes = "192x192", href = "icons/icon-192x192.png"
+    ),
+    tags$link(rel = "icon", type = "image/png", href = "icons/icon-192x192.png"),
+    tags$link(
+      href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap",
+      rel="stylesheet"
+    ),
+    tags$title("shinyDashboard")
+  ),
+  
+  # PAGE HEADER ----
   tags$header(
     class = "app__header app__header--main",
     h1(class = "app__heading", "shinyDashboard"),
